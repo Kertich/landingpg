9cc6c03fce9b9f81a498d5de95468cbb21456143
Kertich <kipsangkertich@gmail.com>
2022-10-18 15:06:35 +0300

    Updates

---
+++ precache-manifest.1c8b020f3fab193053e46d45e74d8fd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273d3fc884e6e271801f80760e4a8e8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65dee4f893be0376b283",
+    "url": "/static/js/2.e47a653f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.e47a653f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b60cfb533e430c6052e4",
+    "url": "/static/js/main.25086eb8.chunk.js"
+  },
+  {
+    "revision": "483004bec81a22457790",
+    "url": "/static/js/runtime-main.75ccab02.js"
+  }
+]);
